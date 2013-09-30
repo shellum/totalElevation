a187574364ad6fe6fad07f0a79faf398c0e9a510
Cameron Shellum <shellum@gmail.com>
2013-09-30 08:04:41 -0600

    Remove old default menu

--- gen/com/finalhack/totalelevation/R.java
+++ gen/com/finalhack/totalelevation/R.java
@@ -16,45 +16,39 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int gps_fix_quality=0x7f070004;
-        public static final int gps_number_of_satellites=0x7f070005;
-        public static final int gps_signal_strength=0x7f070006;
-        public static final int graph=0x7f070007;
-        public static final int imageView1=0x7f070009;
-        public static final int menu_settings=0x7f07000b;
-        public static final int prn_list=0x7f070003;
-        public static final int text_change=0x7f070001;
-        public static final int text_elevation=0x7f070002;
-        public static final int text_lat=0x7f070008;
-        public static final int text_lon=0x7f07000a;
-        public static final int text_start=0x7f070000;
+        public static final int gps_fix_quality=0x7f060004;
+        public static final int gps_number_of_satellites=0x7f060005;
+        public static final int gps_signal_strength=0x7f060006;
+        public static final int graph=0x7f060007;
+        public static final int imageView1=0x7f060009;
+        public static final int prn_list=0x7f060003;
+        public static final int text_change=0x7f060001;
+        public static final int text_elevation=0x7f060002;
+        public static final int text_lat=0x7f060008;
+        public static final int text_lon=0x7f06000a;
+        public static final int text_start=0x7f060000;
     }
     public static final class layout {
         public static final int elevation=0x7f030000;
     }
-    public static final class menu {
-        public static final int elevation=0x7f060000;
-    }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int default_text=0x7f040009;
-        public static final int elevation=0x7f040007;
-        public static final int elevation_change=0x7f040008;
-        public static final int elevation_start=0x7f040006;
-        public static final int gps_disabled=0x7f04000d;
-        public static final int gps_fix_quality=0x7f04000e;
-        public static final int gps_number_of_satellites=0x7f04000c;
-        public static final int gps_signal_strength=0x7f04000b;
-        public static final int hello_world=0x7f040001;
-        public static final int help=0x7f040012;
-        public static final int lat=0x7f040004;
-        public static final int lon=0x7f040005;
-        public static final int menu_settings=0x7f040002;
-        public static final int ok=0x7f040011;
-        public static final int pdop=0x7f040010;
-        public static final int title_activity_elevation=0x7f040003;
-        public static final int visible_sats=0x7f04000f;
-        public static final int waiting=0x7f04000a;
+        public static final int default_text=0x7f040007;
+        public static final int elevation=0x7f040005;
+        public static final int elevation_change=0x7f040006;
+        public static final int elevation_start=0x7f040004;
+        public static final int gps_disabled=0x7f04000b;
+        public static final int gps_fix_quality=0x7f04000c;
+        public static final int gps_number_of_satellites=0x7f04000a;
+        public static final int gps_signal_strength=0x7f040009;
+        public static final int help=0x7f040010;
+        public static final int lat=0x7f040002;
+        public static final int lon=0x7f040003;
+        public static final int ok=0x7f04000f;
+        public static final int pdop=0x7f04000e;
+        public static final int title_activity_elevation=0x7f040001;
+        public static final int visible_sats=0x7f04000d;
+        public static final int waiting=0x7f040008;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
